f0ab3e5950e29d0012559e29a426dcf1df4e6340
robert-er <robert.roza.pl@gmail.com>
2023-01-19 23:55:04 +0100

    [controller] created simple read file endpoint at FileController - logging content of pdf file

---
+++ README.md
@@ -0,0 +1,17 @@
+#Budget Web Application
+
+Application is web app based on REST service with database. App reads pdf files with bank accounts billings and save entries to the database. 
+It will be possible to read PDFs from different banks.
+Duplicated entries should be ignored. Then app will classify expenses and incomes by categories. User can create budget monthly limits for each category and plan monthly budget.
+
+### Used technologies
+* Java 17
+* Spring 3.0.1
+* Apache Log4J 2.11.1
+* Apache PdfBOX 2.0.13
+
+### Endpoints
+
+GET http://localhost:8080/file?filename=path_to_pdf - read PDF files. Returns pdf path. 
+At the moment file is logged by logger.
+
---
+++ src/main/java/com/home/budget/controller/FileController.java
@@ -0,0 +1,76 @@
+package com.home.budget.controller;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("file")
+public class FileController {
+
+    @Autowired
+    private static final org.apache.logging.log4j.Logger log =
+            org.apache.logging.log4j.LogManager.getLogger(FileController.class);
+
+    @GetMapping
+    public ResponseEntity<String> download(@RequestParam("filename") String filename) {
+
+        File file = new File(filename);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+
+        printFile(file);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.getAbsolutePath().length())
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(file.getAbsolutePath());
+    }
+
+    private void printFile(File file) {
+        String[] fileLines = readPDFLines(file);
+
+        for (String line : fileLines) {
+            log.info(line);
+        }
+    }
+
+    private static String[] readPDFLines(File file) {
+
+        String[] lines = new String[0];
+        try (PDDocument document = PDDocument.load(file)) {
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+
+                // split by whitespace
+                lines = pdfFileInText.split("\\r?\\n");
+                document.close();
+            }
+        } catch (IOException e) {
+            log.error("Unable to open file [{}]", file, e);
+        }
+        return lines;
+    }
+}
