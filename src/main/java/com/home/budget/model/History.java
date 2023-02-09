9d008509a56736f952b3650f6c06488d5bdbfe5c
robert-er <robert.roza.pl@gmail.com>
2023-02-09 01:38:24 +0100

    [HistoryServiceImplTest] added service ju tests

--- src/main/java/com/home/budget/model/History.java
+++ src/main/java/com/home/budget/model/History.java
@@ -10,7 +10,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.sql.Date;
+import java.util.Objects;
 
 
 @Entity
@@ -34,7 +35,15 @@ public class History {
     private BigDecimal balanceAfter;
 
     @Builder
-    public History(Date operationDate, Date postingDate, String description, String fromAccount, String toAccount, String cardNumber, String title, BigDecimal amount, BigDecimal balanceAfter) {
+    public History(Date operationDate,
+                   Date postingDate,
+                   String description,
+                   String fromAccount,
+                   String toAccount,
+                   String cardNumber,
+                   String title,
+                   BigDecimal amount,
+                   BigDecimal balanceAfter) {
         this.operationDate = operationDate;
         this.postingDate = postingDate;
         this.description = description;
@@ -44,5 +53,35 @@ public class History {
         this.title = title;
         this.amount = amount;
         this.balanceAfter = balanceAfter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        History history = (History) o;
+        return Objects.equals(operationDate, history.operationDate)
+                && Objects.equals(postingDate, history.postingDate)
+                && Objects.equals(description, history.description)
+                && Objects.equals(fromAccount, history.fromAccount)
+                && Objects.equals(toAccount, history.toAccount)
+                && Objects.equals(cardNumber, history.cardNumber)
+                && Objects.equals(title, history.title)
+                && Objects.equals(amount, history.amount)
+                && Objects.equals(balanceAfter, history.balanceAfter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                operationDate,
+                postingDate,
+                description,
+                fromAccount,
+                toAccount,
+                cardNumber,
+                title,
+                amount,
+                balanceAfter);
     }
 }
--- src/test/java/com/home/budget/service/impl/HistoryServiceImplTest.java
+++ src/test/java/com/home/budget/service/impl/HistoryServiceImplTest.java
@@ -9,8 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Date;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,24 +23,83 @@ public class HistoryServiceImplTest {
 
     @Test
     public void shouldFindById() {
+        //Given
+        History givenHistory = History.builder()
+                .operationDate(Date.valueOf("1988-02-19"))
+                .postingDate(Date.valueOf("1988-02-22"))
+                .description("PRL")
+                .fromAccount("22113344")
+                .toAccount("88777666555")
+                .title("Wynagrodzenie")
+                .amount(new BigDecimal("1000.00"))
+                .balanceAfter(new BigDecimal("500.00"))
+                .build();
+        History savedHistory = service.add(givenHistory);
+        Long id = savedHistory.getId();
 
+        //When
+        History retrievedHistory = service.findById(id).get();
+
+        //Then
+        assertEquals(givenHistory.getOperationDate(), retrievedHistory.getOperationDate());
+        assertEquals(givenHistory.getPostingDate(), retrievedHistory.getPostingDate());
+        assertEquals(givenHistory.getDescription(), retrievedHistory.getDescription());
+        assertEquals(givenHistory.getFromAccount(), retrievedHistory.getFromAccount());
+        assertEquals(givenHistory.getToAccount(), retrievedHistory.getToAccount());
+        assertEquals(givenHistory.getTitle(), retrievedHistory.getTitle());
+        assertEquals(givenHistory.getAmount(), retrievedHistory.getAmount());
+        assertEquals(givenHistory.getBalanceAfter(), retrievedHistory.getBalanceAfter());
     }
 
     @Test
     public void shouldGetAll() {
+        //Given
+        History givenHistory1 = History.builder()
+                .operationDate(Date.valueOf("2022-12-12"))
+                .postingDate(Date.valueOf("2022-12-14"))
+                .description("Przelew testowy")
+                .fromAccount("77 8888 2835 0000 0001 4238 9988")
+                .toAccount("10 1001 2529 0000 0001 5189 2222")
+                .title("Przelew 1")
+                .amount(new BigDecimal("123.45"))
+                .balanceAfter(new BigDecimal("845.87"))
+                .build();
+        History savedHistory1 = service.add(givenHistory1);
+
+        History givenHistory2 = History.builder()
+                .operationDate(Date.valueOf("2021-11-15"))
+                .postingDate(Date.valueOf("2021-11-16"))
+                .description("Przelew testowy 2")
+                .fromAccount("66 8888 2835 0000 0001 4238 6666")
+                .toAccount("66 6666 2529 0000 6666 5189 2222")
+                .title("Przelew 2")
+                .amount(new BigDecimal("222123.45"))
+                .balanceAfter(new BigDecimal("222845.87"))
+                .build();
+        History savedHistory2 = service.add(givenHistory2);
+
+        //When
+        List<History> retrievedList = service.getAll();
+
+        //Then
+        assertTrue(retrievedList.stream()
+                .filter(e -> e.getDescription().equals("Przelew testowy"))
+                .anyMatch(e -> e.equals(savedHistory1)));
+        assertTrue(retrievedList.stream()
+                .filter(e -> e.getAmount().compareTo(new BigDecimal("222123.45")) == 0)
+                .allMatch(e -> e.equals(savedHistory2)));
     }
 
     @Test
-    public void shouldAdd() throws ParseException {
+    public void shouldAdd() {
         //Given
-        SimpleDateFormat formatter = new SimpleDateFormat("yyy-MM-dd");
         History givenHistory = History.builder()
-                .operationDate(formatter.parse("2023-01-26"))
-                .postingDate(formatter.parse("2023-01-26"))
+                .operationDate(Date.valueOf("2023-01-26"))
+                .postingDate(Date.valueOf("2023-01-26"))
                 .description("UZNANIE")
                 .fromAccount("57 1090 2835 0000 0001 4238 2303")
-                .toAccount(" 12 1090 2529 0000 0001 5189 3244")
-                .title(" Promocja Godna Polecenia 1 23")
+                .toAccount("12 1090 2529 0000 0001 5189 3244")
+                .title("Promocja Godna Polecenia 1 23")
                 .amount(new BigDecimal("25.63"))
                 .balanceAfter(new BigDecimal("1718.44"))
                 .build();
